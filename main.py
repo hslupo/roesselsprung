02930b185fe42717b82fb5fde67e02ea358399ae
hslupo <hslupo@gmail.com>
2022-07-17 21:12:50 +0200

    mit pyGame als GUI

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # https://www.raetselstunde.de/kunterbunt/roessel-sprung/roesselsprung-015.html
+import pygame as pg
+
 
 spalten = zeilen = 0
 rätsel = {}
 sprungmatrix = [(-2,-1), (-2,1), (-1,2), (1,2),(2,1), (2,-1), (-1,-2), (1,-2)]
+richtungen = {pg.K_DOWN: (0, 1), pg.K_UP: (0, -1), pg.K_LEFT: (-1, 0), pg.K_RIGHT: (1, 0)}
+
+
 
 def raetsel_laden(datei):
     global zeilen, spalten, rätsel
@@ -10,7 +15,7 @@ def raetsel_laden(datei):
         ra = [x for x in f.read().split('\n')]
         zeilen = ra.__len__()
         spalten = ra[0].split().__len__()
-        rätsel = {(z, s): [ra[z].split()[s], ' '] for s in range(spalten) for z in range(zeilen)}
+        rätsel = {(s, z): [ra[s].split()[z], ' '] for s in range(zeilen) for z in range(spalten)}
 
 
 def feld_add(sp1, ze1, sp2, ze2):
@@ -74,7 +79,6 @@ def xGehtZu(von, nach):
 
     doku(von)
 
-
 def wer_hat_wieviel_ziele():
     ziele0 = []
     ziele1 = []
@@ -103,22 +107,76 @@ def löscheTupelAusListe(liste, element):
 def löscheZiel(zelle, ziel):
     rätsel[zelle] = löscheTupelAusListe(rätsel[zelle], ziel)
 
+def zeichne_brett(board):
+  br = hö = raster - rand
+  for sp, ze in board:
+    x, y = sp*raster+rand, ze*raster+rand
+    if aktiveZelle == (sp, ze):
+        rahmen = (255, 50, 50)
+    else:
+        rahmen = (255, 255, 255)
+    pg.draw.rect(screen, "lightblue",(x, y, br, hö))
+    pg.draw.rect(screen, rahmen, (x, y, br, hö), 2)
+    if board[(ze,sp)][0] == '': continue
+    zeichne_text(str(board[(ze,sp)][0]), raster//3, (x+br//2, y+hö//2), (25, 25, 25))
 
+
+def zeichne_text(text, font_size, pos, farbe):
+  t = pg.font.SysFont('Arial', font_size).render(text, False, farbe)
+  t_rect = t.get_rect(center=pos)
+  screen.blit(t, t_rect)
+
+def aktiveZelleVerschieben(key):
+    global aktiveZelle, zeilen, spalten
+    sp, ze = aktiveZelle
+
+    if key == pg.K_DOWN:
+        if ze < zeilen - 1:
+            ze += 1
+        else:
+            ze = 0
+    elif key == pg.K_UP:
+        if ze > 0:
+            ze -= 1
+        else:
+            ze = zeilen - 1
+    elif key == pg.K_RIGHT:
+        if sp < spalten - 1:
+            sp += 1
+        else:
+            sp = 0
+    elif key == pg.K_LEFT:
+        if sp > 0:
+            sp -= 1
+        else:
+            sp = spalten - 1
+    aktiveZelle = (sp, ze)
 
 if __name__ == '__main__':
     raetsel_laden("roesselsprung-015.bsp")
     sprungliste_erzeugen()
     ergebnisse = []
-    parse_zelle(list(rätsel.keys())[0],1)
-    print(ergebnisse)
 
-    xGehtZu((0, 0), (1, 2))
-    #print(werGehtZu((0, 0)))
-    # print(0,0)
-    #z0, z1, z2, z3, z4 = wer_hat_wieviel_ziele()
-    #print(f'keine Ziele {z0}\n  1 Ziel {z1}\n    2 Ziele {z2}\n     3 Ziele {z3}\n der  Rest {z4}')
-    #xGehtZu((0, 0), (1, 2))
-    #z0, z1, z2, z3, z4 = wer_hat_wieviel_ziele()
-    #print(f'keine Ziele {z0}\n  1 Ziel {z1}\n    2 Ziele {z2}\n    3 Ziele {z3}\n   der Rest {z4}')
-    #print(werGehtZu((0, 0)))
+    pg.init()
+    raster = 120
+    rand = 0  # raster // 8
+    screen = pg.display.set_mode((raster * zeilen + rand, raster * spalten + rand))
+    pg.display.set_caption("Rösselsprung Texträtsel")
+    weitermachen = True
+    clock = pg.time.Clock()
+    aktiveZelle = (0, 0)
 
+
+    while weitermachen:
+        clock.tick(40)
+        for ereignis in pg.event.get():
+            if ereignis.type == pg.QUIT:
+                weitermachen = False
+            if ereignis.type == pg.KEYDOWN and ereignis.key in richtungen:
+                aktiveZelleVerschieben(ereignis.key)
+                print(aktiveZelle)
+        # screen.fill('#EEEEEE')
+        zeichne_brett(rätsel)
+
+        pg.display.flip()
+    pg.quit()
---
+++ raetsel.py
@@ -0,0 +1,98 @@
+import pygame as pg
+import random as rnd
+
+
+def zeichne_brett(board):
+  br = hö = raster - rand
+  for ze,sp in board:
+    x, y = ze*raster+rand, sp*raster+rand
+    pg.draw.rect(screen, farben[board[(ze,sp)]], (x, y, br, hö), border_radius=16)
+    if board[(ze,sp)] == 0: continue
+    zeichne_text(str(board[(ze,sp)]), raster//3, (x+br//2, y+hö//2), (255, 255, 255))
+
+
+def zeichne_text(text, font_size, pos, farbe):
+  t = pg.font.SysFont('Arial', font_size).render(text, False, farbe)
+  t_rect = t.get_rect(center=pos)
+  screen.blit(t, t_rect)
+
+
+def setze_neue_2(board):
+  freie_felder = [pos for pos in board if board[pos] == 0]
+  if not freie_felder: return False
+  pos = rnd.choice(freie_felder)
+  board[pos] = 2
+  return True
+
+
+def verschiebe(key, board, score):
+  x_delta, y_delta = richtungen[key]
+  while True:
+    change = False
+    for (x, y), wert in board.items():
+      zu_pos = (x+x_delta, y+y_delta)
+      if zu_pos not in board or wert == 0:
+        continue
+      if board[zu_pos] == 0 or board[zu_pos] == wert:
+        change = True
+        board[(x, y)] = 0
+        if board[zu_pos] == 0:
+          board[zu_pos] = wert
+        else:
+          board[zu_pos] = wert * 2
+          score += wert * 2
+    if not change:
+      return score
+
+
+def monte_carlo(anz):
+  erg = []
+  for r in range(4):
+    summe = 0
+    for i in range(anz):
+      b = brett.copy()
+      neuer_score = verschiebe(keys[r], b, score)
+      while setze_neue_2(b):
+        neuer_score = verschiebe(rnd.choice(keys), b, neuer_score)
+      summe += neuer_score
+    erg.append([summe, r])
+  return keys[sorted(erg)[-1][1]]
+
+
+richtungen = {pg.K_DOWN: (0, 1), pg.K_UP: (0, -1), pg.K_LEFT: (-1, 0), pg.K_RIGHT: (1, 0)}
+keys = [k for k in richtungen]
+farben = {0: '#CDC0B4', 2: '#EEE4DA', 4: '#EDE0C8', 8: '#EDE0C8', 16: '#F59563',
+          32: '#F67C60', 64: '#F65E3B', 128: '#EDCF73', 256: '#EDCC62',
+          512: '#EDC850', 1024: '#EDC850', 2048: '#EDC22D', 4096: '#000000', 8192: '#000000'}
+
+zeilen = 4
+spalten = 4
+
+brett = {(x, y): 0 for x in range(zeilen) for y in range(spalten)}
+score, spielstatus = 0, True
+setze_neue_2(brett)
+
+pg.init()
+raster = 100
+rand = 0  #raster // 8
+screen = pg.display.set_mode((raster*zeilen+rand, raster*spalten+rand))
+weitermachen = True
+clock = pg.time.Clock()
+
+while weitermachen:
+  clock.tick(40)
+  for ereignis in pg.event.get():
+    if ereignis.type == pg.QUIT:
+      weitermachen = False
+    if ereignis.type == pg.KEYDOWN and ereignis.key in richtungen:
+      score = verschiebe(ereignis.key, brett, score)
+      setze_neue_2(brett)
+  screen.fill('#BBADA0')
+  zeichne_brett(brett)
+  # hier spielt die KI!!!!!
+  if spielstatus:
+    score = verschiebe(monte_carlo(50), brett, score)
+    if not setze_neue_2(brett):
+      spielstatus = False
+  pg.display.flip()
+pg.quit()
